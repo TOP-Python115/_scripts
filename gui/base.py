630e737d90935d4e983f0f2e6834e04e08bc1804
Gennadiy <renardes@gmail.com>
2022-09-22 23:30:48 +0500

    tkinter: базовое приложение

---
+++ gui/base.py
@@ -0,0 +1,45 @@
+import tkinter
+import tkinter.ttk as ttk
+
+
+def btn_on_click():
+    text_blabel.set(text_field.get())
+
+
+root = tkinter.Tk()
+root.title('Tk Demonstrator')
+
+mainframe = ttk.Frame(root, width=400, height=300, padding=5)
+mainframe.grid(row=0, column=0, sticky='nsew')
+
+ttk.Label(
+    mainframe,
+    text='Введите текст: ',
+    font=('Arounder', 26)
+).grid(row=0, column=0, sticky='nw')
+
+text_field = tkinter.StringVar()
+ttk.Entry(
+    mainframe,
+    textvariable=text_field,
+    font=('Hack', 20)
+).grid(row=1, column=0, sticky='nwe')
+
+btn = ttk.Button(mainframe, text='\u2193', command=btn_on_click)
+btn.grid(row=1, column=1, sticky='nse', padx=(6, 0))
+
+text_blabel = tkinter.StringVar()
+ttk.Label(
+    mainframe,
+    textvariable=text_blabel,
+    font=('Hack', 26), background='grey'
+).grid(row=2, column=0, columnspan=2, sticky='nsew', pady=5)
+
+root.rowconfigure(0, weight=1)
+root.columnconfigure(0, weight=1)
+mainframe.rowconfigure(2, weight=1)
+mainframe.columnconfigure(0, weight=1)
+
+root.bind('<Return>', lambda event: btn.invoke())
+
+root.mainloop()
