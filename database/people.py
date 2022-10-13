caef64eb7c1e857617f35145ecd490036b758f97
Gennadiy <renardes@gmail.com>
2022-10-13 23:38:11 +0500

    имена людей

---
+++ database/people.py
@@ -0,0 +1,122 @@
+data = [
+	{
+		'name': "Otto",
+		'surname': "Dyer"
+	},
+	{
+		'name': "Mechelle",
+		'surname': "Freeman"
+	},
+	{
+		'name': "Olympia",
+		'surname': "Bush"
+	},
+	{
+		'name': "Isadora",
+		'surname': "Freeman"
+	},
+	{
+		'name': "Ainsley",
+		'surname': "Kerr"
+	},
+	{
+		'name': "Riley",
+		'surname': "Brewer"
+	},
+	{
+		'name': "Hermione",
+		'surname': "Pruitt"
+	},
+	{
+		'name': "Deacon",
+		'surname': "Mccall"
+	},
+	{
+		'name': "Meredith",
+		'surname': "Horton"
+	},
+	{
+		'name': "Talon",
+		'surname': "Hardy"
+	},
+	{
+		'name': "Alexa",
+		'surname': "Rollins"
+	},
+	{
+		'name': "Kennedy",
+		'surname': "Garcia"
+	},
+	{
+		'name': "Buckminster",
+		'surname': "Byers"
+	},
+	{
+		'name': "Igor",
+		'surname': "Byers"
+	},
+	{
+		'name': "Adrian",
+		'surname': "Morin"
+	},
+	{
+		'name': "Robert",
+		'surname': "Roth"
+	},
+	{
+		'name': "John",
+		'surname': "Carr"
+	},
+	{
+		'name': "Gemma",
+		'surname': "Robinson"
+	},
+	{
+		'name': "Maia",
+		'surname': "Stokes"
+	},
+	{
+		'name': "Ifeoma",
+		'surname': "Jenkins"
+	},
+	{
+		'name': "Isabelle",
+		'surname': "Delacruz"
+	},
+	{
+		'name': "Jared",
+		'surname': "Henson"
+	},
+	{
+		'name': "Willa",
+		'surname': "Barber"
+	},
+	{
+		'name': "Palmer",
+		'surname': "Black"
+	},
+	{
+		'name': "Lillith",
+		'surname': "Pitts"
+	},
+	{
+		'name': "Graham",
+		'surname': "Rowland"
+	},
+	{
+		'name': "Oren",
+		'surname': "Hayes"
+	},
+	{
+		'name': "Bert",
+		'surname': "Britt"
+	},
+	{
+		'name': "Amery",
+		'surname': "Fleming"
+	},
+	{
+		'name': "Zoe",
+		'surname': "Mullins"
+	}
+]
