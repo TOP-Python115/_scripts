157f033ed3504382f04764a9f9f420e09e0b17bb
Gennadiy <renardes@gmail.com>
2022-10-27 21:46:41 +0500

    второй проект Django: academy

--- django_project/first_site/settings.py
+++ django_project/first_site/settings.py
@@ -33,6 +33,7 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'helloworld.apps.HelloworldConfig',
+    'academy.apps.AcademyConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
