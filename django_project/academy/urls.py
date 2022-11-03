36fc9cfdd775f2a75346f4c669109424ed0bac0d
Gennadiy <renardes@gmail.com>
2022-11-03 21:57:30 +0500

    academy: использование в CBV связи с моделью

--- django_project/academy/urls.py
+++ django_project/academy/urls.py
@@ -2,11 +2,12 @@ from django.urls import path
 
 from .models import Faculty
 from .fbv_views import institutes_list, faculty_main
-from .views import About
+from .views import About, FacultiesView
 
 
 urlpatterns = [
-    path('', institutes_list, name='academy_main'),
+    # path('', institutes_list, name='academy_main'),
+    path('', FacultiesView.as_view(), name='academy_main'),
     path('about', About.as_view(), name='academy_about')
 ]
 
--- django_project/academy/views.py
+++ django_project/academy/views.py
@@ -1,10 +1,23 @@
-from django.views.generic import TemplateView
+from django.views.generic import TemplateView, ListView
 
 from django.http import HttpResponse
+
+from .models import Faculty
 
 
 class About(TemplateView):
     def get(self, request, *args, **kwargs):
         return HttpResponse('History of Academy')
 
+
+class FacultiesView(ListView):
+    model = Faculty
+
+    def get(self, request, *args, **kwargs):
+        document = '<h2>Институты университета</h2>\n'
+        document += '<ol>\n'
+        for obj in self.model.objects.all():
+            document += f'\t<li><a href=/academy/{obj!r}>{obj!s}</a></li>\n'
+        document += '</ol>'
+        return HttpResponse(document)
 
