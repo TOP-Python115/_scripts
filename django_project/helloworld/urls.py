d95a593d8125e45728282e436a6b5a1489fd4dcf
Gennadiy <renardes@gmail.com>
2022-11-10 21:48:32 +0500

    helloworld: основная последовательность работы с шаблоном
    явное создание экземпляров классов Engine, Template и Context

--- django_project/helloworld/urls.py
+++ django_project/helloworld/urls.py
@@ -3,5 +3,5 @@ from django.urls import path
 from .views import helloworld
 
 urlpatterns = [
-    path('index', helloworld, name='hw-main')
+    path('', helloworld, name='hw-main')
 ]
--- django_project/helloworld/views.py
+++ django_project/helloworld/views.py
@@ -1,6 +1,28 @@
 from django.shortcuts import render
+
+from django.template import Template, Context, Engine
+
 from django.http import HttpResponse
 
 
 def helloworld(request):
-    return HttpResponse("Hello world!\nYou're on main page of this site.")
+    # строковый шаблон
+    content = """
+    <h2>{{ title }}</h2>
+    <p><i>{{ lead }}</i></p>
+    """
+    # движок шаблонизатора — по умолчанию используется первый корректно загруженный из TEMPLATES в модуле проекта settings
+    engine = Engine()
+    # объект шаблона — дерево
+    template = Template(content, engine=engine)
+    # объект контекста — стек (словароподобный)
+    context = Context({
+        'title': 'Главная страница',
+        'lead': 'Приветствуем вас на сайте helloworld!'
+    })
+    # context['text'] = 'Описание проекта...'
+    # рендер шаблона — подстановка значений из контекста в шаблон и генерация окончательного документа
+    document = template.render(context)
+    print(document)
+    print(type(document))
+    return HttpResponse(document)
