40945371dd59776c63ad713b428398541adf5261
Gennadiy <renardes@gmail.com>
2022-11-17 20:50:38 +0500

    подключение django-crispy-forms, crispy-bootstrap5 и CDN подключение Bootstrap 5

--- django_project/first_site/settings.py
+++ django_project/first_site/settings.py
@@ -34,6 +34,8 @@ ALLOWED_HOSTS = []
 INSTALLED_APPS = [
     'helloworld.apps.HelloworldConfig',
     'academy.apps.AcademyConfig',
+    'crispy_forms',
+    'crispy_bootstrap5',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -124,3 +126,6 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+CRISPY_ALLOWED_TEMPLATE_PACKS = 'bootstrap5'
+CRISPY_TEMPLATE_PACK = 'bootstrap5'
